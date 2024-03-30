776993b1e1ffe71f7b21dcb4cb84c130298d11be
Rami339 <elanggarami271@gmail.com>
2024-03-30 21:37:47 +0200

    add background native splash

---
+++ lib/todo_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todo/features/home/ui/home_screen.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+
+    );
+  }
+}
